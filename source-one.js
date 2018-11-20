d0fe2b387cf84dff42113353a4d7abae9eae4227
Валерий Колесник <mypowerismind@gmail.com>
2018-11-20 23:59:15 +0200

    Add sample js file

---
+++ source-one.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(e){
+    var addr = document.querySelectorAll("[data-type=address]");
+    var $copyFrom = addr[0];
+    var $copyTo = [addr[2], addr[3], addr[4]];
+    debugger;
+}, false);
