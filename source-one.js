1f65bc7ecab695e99b7102b9b8ce1ae6c1484444
Валерий Колесник <mypowerismind@gmail.com>
2018-11-21 21:09:29 +0200

    Add event listener to all required address sections

--- source-one.js
+++ source-one.js
@@ -1,9 +1,32 @@
+var getAllAddresses = function(){
+    var total = document.querySelectorAll("[data-type=address]");
+    return [total[0],total[2],total[3],total[4]];
+}
+
 window.onload = function() {
-    var addr = document.querySelectorAll("[data-type=address]");
-    var $copyFrom = addr[0];
-    var $copyTo = [addr[2], addr[3], addr[4]];
+
+    getAllAddresses().forEach(function(el, ind){
+        el.addEventListener('change', function(e){
+            var allAddr = getAllAddresses();
+            var parentAddr = e.target.parentElement.parentElement;
+            var currentField = e.target;
+
+            debugger;
+
+            for (var i = 0; i < allAddr.length; i++){
+                var nextAddr = allAddr[i];
+                if (parentAddr == nextAddr) continue;
+                debugger;
+            }
+
+        }, false);
+    });
+
+    // var addr = getAllAddresses;
+    // var $copyFrom = addr[0];
+    // var $copyTo = [addr[2], addr[3], addr[4]];
     
-    $copyFrom.addEventListener('change', function(e){
-        debugger;
-    }, false);
+    // $copyFrom.addEventListener('change', function(e){
+    //     debugger;
+    // }, false);
 };
