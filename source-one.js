bedf0066382145fa4ba4b4d104267013c781443b
Валерий Колесник <mypowerismind@gmail.com>
2018-11-22 09:53:23 +0200

    Add change eventhandler to country input

--- source-one.js
+++ source-one.js
@@ -6,13 +6,27 @@ var getAllAddresses = function(){
 window.onload = function() {
 
     getAllAddresses().forEach(function(el, ind){
+        var countryField = el.querySelector("[data-index=6]");
+        
+        countryField.onchange = function(e){
+            var parentAddr = e.target.parentElement;
+            var countryInput = e.target.querySelector("input");
+            var allAddr = getAllAddresses();
+
+            for (var i = 0; i < allAddr.length; i++){
+                var nextAddr = allAddr[i];
+                debugger;
+                if (parentAddr == nextAddr) continue;
+                var fieldToChange = nextAddr.querySelector("[data-index='6']").querySelector("input");
+                fieldToChange.value = countryInput.value;
+            }
+        };
+
         el.addEventListener('change', function(e){
             var allAddr = getAllAddresses();
             var parentAddr = e.target.parentElement.parentElement;
             var currentField = e.target;
-
-            debugger;
-
+            
             for (var i = 0; i < allAddr.length; i++){
                 var nextAddr = allAddr[i];
                 if (parentAddr == nextAddr) continue;
