6a4bfdbee511585ddd84ea68414ad06bd136f3e3
Валерий Колесник <mypowerismind@gmail.com>
2018-11-29 00:44:51 +0200

    Add fax auto-fill function

--- source-one.js
+++ source-one.js
@@ -7,6 +7,22 @@ var getAllEmails = function(){
     return document.querySelectorAll("input[type='email']");
 }
 
+var getAllFaxFields = function(){
+    var phonesAndFaxes = document.querySelectorAll("[data-type='phone'] > label[data-role='label']");
+    var faxes = [];
+    phonesAndFaxes.forEach(function(el){
+        if (el.innerText.toLowerCase() == "fax"){
+            faxes.push(el);
+        }
+    });
+    console.log(faxes);
+    var result = [];
+    faxes.forEach(function(el){
+        result.push(el.parentElement.querySelector("input"));
+    });
+    return result;
+}
+
 window.onload = function() {
 
     getAllAddresses().forEach(function(el, ind){
@@ -40,14 +56,18 @@ window.onload = function() {
         }, false);
     });
 
-    getAllEmails()[0].addEventListener('change', function(e){
-        var allInputs = getAllEmails();
-        var currentInput = e.target;
+    var autoFillFunctions = [getAllEmails, getAllFaxFields];
 
-        for (var i = 0; i < allInputs.length; i++){
-            var nextInput = allInputs[i];
-            if (nextInput == currentInput) continue;
-            nextInput.value = currentInput.value;
-        }
+    autoFillFunctions.forEach(function(func){
+        func()[0].addEventListener('change', function(e){
+            var allInputs = func();
+            var currentInput = e.target;
+    
+            for (var i = 0; i < allInputs.length; i++){
+                var nextInput = allInputs[i];
+                if (nextInput == currentInput) continue;
+                nextInput.value = currentInput.value;
+            }
+        });
     });
 };
