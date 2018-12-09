ab8291e9de27c68d139ff5993312763729487e55
Валерий Колесник <mypowerismind@gmail.com>
2018-12-09 17:00:27 +0200

    Handle change event for country input

--- source-one.js
+++ source-one.js
@@ -3,6 +3,10 @@ var EVENT_NAMES = {
     INPUT   : 'input'
 };
 
+var getCountryInput = function(elem){
+    return elem.querySelector(COUNTRY_SELECTOR).querySelector("input");
+}
+
 var getAllAddresses = function(){
     var total = document.querySelectorAll("[data-type='address']");
     return [total[0],total[2],total[3],total[4]];
@@ -98,33 +102,36 @@ var SendEvent = function(eventName, htmlElement){
 
 window.onload = function() {
 
-    [getAllAddresses()[0]].forEach(function(el, ind){
-        var countryField = el.querySelector("[data-index='6']");
-        
+    var allAddresses = getAllAddresses();
+
+    [allAddresses[0]].forEach(function(el, ind){
+  
+        var COUNTRY_SELECTOR = "[data-index='6']";
+
+        var countryField = getCountryInput(el);
         var eventType = EVENT_NAMES.CHANGE;
 
-        countryField.onchange = function(e){
-            var parentAddr = e.target.parentElement.parentElement;
-            var countryInput = e.target.querySelector("input");
-            var allAddr = getAllAddresses();
+        countryField.addEventListener(eventType, function(e){
+            var parentAddr = e.target.parentElement.parentElement.parentElement;
+            var countryInput = e.target;
 
-            for (var i = 0; i < allAddr.length; i++){
-                var nextAddr = allAddr[i];
+            for (var i = 0; i < allAddresses.length; i++){
+                var nextAddr = allAddresses[i];
                 if (parentAddr == nextAddr) continue;
-                var fieldToChange = nextAddr.querySelector("[data-index='6']").querySelector("input");
+                
+                var fieldToChange = getCountryInput(nextAddr);
                 fieldToChange.value = countryInput.value;
                 
                 SendEvent(eventType, fieldToChange);
             }
-        };
+        });
 
         el.addEventListener(eventType, function(e){
-            var allAddr = getAllAddresses();
             var parentAddr = e.target.parentElement.parentElement;
             var currentField = e.target;
             
-            for (var i = 0; i < allAddr.length; i++){
-                var nextAddr = allAddr[i];
+            for (var i = 0; i < allAddresses.length; i++){
+                var nextAddr = allAddresses[i];
                 if (parentAddr == nextAddr) continue;
                 var fieldToChange = nextAddr.querySelector("input[data-index='"+currentField.getAttribute("data-index")+"']");
                 fieldToChange.value = currentField.value;                
